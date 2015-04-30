e05b815a1e5ba5d2da09e58bd68c651f0316ade6
ataka <masayuki.ataka@gmail.com>
2015-05-01 07:59:57 +0900

    Refactor function chatwork-insert-tag-to

--- chatwork.el
+++ chatwork.el
@@ -408,29 +408,24 @@ DATA should be decoded with `html-hexify-string' if they contains multibyte."
 
 (defun chatwork-insert-tag-to (member)
   (interactive (list (completing-read "To: " `(,@chatwork-member-alist ,@chatwork-member-alias-alist))))
-  (let (account-id member-info)
+  (let ((format-to (format "[To:%%s] %s%%s%s"
+                           chatwork-to-tag-prefix
+                           chatwork-to-tag-suffix))
+        account-id member-info)
     (cond
      ((setq member-info (assoc member chatwork-member-alist))
       (setq account-id (cdr member-info))
-      (insert (format "[To:%s] %s%s%s\n" account-id
-                      chatwork-to-tag-prefix
-                      (chatwork-member-name-by-account-id account-id)
-                      chatwork-to-tag-suffix)))
+      (insert (format format-to account-id (chatwork-member-name-by-account-id account-id))
+              "\n"))
      ((numberp (cdr (setq member-info (assoc member chatwork-member-alias-alist))))
       (setq account-id (cdr member-info))
-      (insert (format "[To:%s] %s%s%s\n" account-id
-                      chatwork-to-tag-prefix
-                      (chatwork-member-name-by-account-id account-id)
-                      chatwork-to-tag-suffix)))
+      (insert (format format-to account-id (chatwork-member-name-by-account-id account-id))
+              "\n"))
      ((listp (cdr member-info))
       (mapc (lambda (account-id)
               (when (or (not (eq major-mode 'chatwork-mode))
                         (rassoc account-id chatwork-member-alist))
-                (insert (format "[To:%s] %s%s%s%s" account-id
-                                chatwork-to-tag-prefix
-                                (chatwork-member-name-by-account-id account-id)
-                                chatwork-to-tag-suffix
-                                chatwork-member-separator))))
+                (insert (format format-to account-id (chatwork-member-name-by-account-id account-id)))))
             (cdr member-info))
       (delete-char (- (length chatwork-member-separator)))
       (insert "\n"))
